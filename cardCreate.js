fafe2864d459d6b282ac94b18c10d5b6555380ba
Nathan <natwave2@gmail.com>
2017-10-21 14:15:42 -0600

    commit

--- cardCreate.js
+++ cardCreate.js
@@ -13,11 +13,11 @@ inquirer.prompt([
 		message: "Would you like to create or review flash cards?",
 		choices: ["Create some flash cards", "Review some flash cards"]
 
-	}]).then(function(gameMode) {
-		
+	}]).then(function (gameMode) {
+
 		if (gameMode.gameMode === "Create some flash cards") {
 			cardCreation();
-		
+
 		} else if (gameMode.gameMode === "Review some flash cards") {
 			reviewCards();
 		}
@@ -26,91 +26,91 @@ inquirer.prompt([
 function cardCreation() {
 
 	inquirer.prompt([
-	    {
-	     	type: "list",
-	     	name: "flashCardType",
-	     	message: "Select a style of flash card deck to build.",
-	     	choices: ["Basic Card", "Cloze Card"]
-	    },
-	    {
-	    	type: "input",
-	    	name: "numberOfCards",
-	    	message: "How many cards would you like to make?",
-	    }
-	]).then(function(cardStyle) {
-	    
-	    var gameType = cardStyle.flashCardType;
-	    var cardCount = cardStyle.numberOfCards;
-	    
-	    cardCreator();
+		{
+			type: "list",
+			name: "flashCardType",
+			message: "Select a style of flash card deck to build.",
+			choices: ["Basic Card", "Cloze Card"]
+		},
+		{
+			type: "input",
+			name: "numberOfCards",
+			message: "How many cards would you like to make?",
+		}
+	]).then(function (cardStyle) {
 
-	    function cardCreator() {
+		var gameType = cardStyle.flashCardType;
+		var cardCount = cardStyle.numberOfCards;
 
-		    if (gameType === "Cloze Card" && count < cardCount){
-		        	
-		        console.log("Cloze Flashcard Creator:");
-			    	
-			    inquirer.prompt([
-			        {
-			        	type: "input",
-			        	name: "fullText",
-			        	message: "Enter a fact that you would like to remember."
-			        },
-			        {
-			        	type: "input",
-			        	name: "clozeDeletion",
-			        	message: "Re-Enter the words from the fact that you want hidden."
-			        	
-			    }]).then(function(answers) {
+		cardCreator();
 
-			        var newCard = new clozeData.ClozeCard(
-			        	answers.fullText,
-			        	answers.clozeDeletion,
-			        	answers.partialText);
+		function cardCreator() {
 
-    				fs.appendFile("clozeLog.txt", "\n" + JSON.stringify(newCard) + ",", function(err) {
-			    		if (err) {
-			    			return console.log("Error; " + err);
-			    		}
-			    	})
+			if (gameType === "Cloze Card" && count < cardCount) {
 
-			        count++
+				console.log("Cloze Flashcard Creator:");
 
-			        cardCreator();
+				inquirer.prompt([
+					{
+						type: "input",
+						name: "fullText",
+						message: "Enter a fact that you would like to remember."
+					},
+					{
+						type: "input",
+						name: "clozeDeletion",
+						message: "Re-Enter the words from the fact that you want hidden."
 
-			    });
-			    
-			} else if(gameType === "Basic Card" && count < cardCount){
+					}]).then(function (answers) {
 
-			    console.log("Basic Flashcard Creater:");
+						var newCard = new clozeData.ClozeCard(
+							answers.fullText,
+							answers.clozeDeletion,
+							answers.partialText);
 
-			    inquirer.prompt([
-			    	{
-			    		type: "input",
-			    		name: "front",
-			    		message: "Enter a question."
-			    	},
-			    	{
-			    		type: "input",
-			    		name: "back",
-			    		message: "Enter the answer."
-			    }]).then(function(answers) {
+						fs.appendFile("clozeLog.txt", "\n" + JSON.stringify(newCard) + ",", function (err) {
+							if (err) {
+								return console.log("Error; " + err);
+							}
+						})
 
-			    	var newCard = new basicData.BasicCard(
-			    		answers.front,
-			    		answers.back);
+						count++
 
-			    	fs.appendFile("basicLog.txt", "" + JSON.stringify(newCard) + "," + "\n", function(err) {
-			    		if (err) {
-			    			return console.log("Error; " + err);
-			    		}
-			    	})
+						cardCreator();
 
-			    	count++
+					});
 
-			    	cardCreator();
+			} else if (gameType === "Basic Card" && count < cardCount) {
 
-			    })
+				console.log("Basic Flashcard Creater:");
+
+				inquirer.prompt([
+					{
+						type: "input",
+						name: "front",
+						message: "Enter a question."
+					},
+					{
+						type: "input",
+						name: "back",
+						message: "Enter the answer."
+					}]).then(function (answers) {
+
+						var newCard = new basicData.BasicCard(
+							answers.front,
+							answers.back);
+
+						fs.appendFile("basicLog.txt", "" + JSON.stringify(newCard) + "," + "\n", function (err) {
+							if (err) {
+								return console.log("Error; " + err);
+							}
+						})
+
+						count++
+
+						cardCreator();
+
+					})
 			} else if (count = cardCount) {
 				reviewCards();
 			}
@@ -127,7 +127,7 @@ function reviewCards() {
 			message: "Which set of flash cards would you like to review?",
 			choices: ["Basic Cards", "Cloze Cards"]
 		},
-	]).then(function(reviewType) {
+	]).then(function (reviewType) {
 
 		reviewDeck();
 
@@ -135,60 +135,27 @@ function reviewCards() {
 
 			if (reviewType.reviewType === "Basic Cards") {
 
-				fs.readFile("basiclog.txt", function(err, data) {
+				fs.readFile("basiclog.txt", function (err, data) {
 
 					var logArray = data.toString().split(",");
 					var deckQuestionArray = [];
 					var deckAnswerArray = [];
 
-					for (var i = 0; i <logArray.length; i+=2) {
+					for (var i = 0; i < logArray.length; i += 2) {
 						deckQuestionArray.push(logArray[i].replace("}", ""));
 					};
 
-					for (var i = 1; i <logArray.length; i+=2) {
+					for (var i = 1; i < logArray.length; i += 2) {
 						deckAnswerArray.push(logArray[i].replace("}", ""));
 					};
 
-				inquirer.prompt([
-			  		{
-			  			name: "userDisplay",
-			  			message: deckQuestionArray[reviewCount],
-			  		}]).then(function(userResponse){
-			          	
-			          	if ('"back"'+":"+'"'+userResponse.userDisplay+'"' == deckAnswerArray[reviewCount]){
-			            	console.log("YES!");
-			         	 } else {
-			            	console.log("NO! " + deckAnswerArray[reviewCount]);
-			         	 }
+					inquirer.prompt([
+						{
+							name: "userDisplay",
+							message: deckQuestionArray[reviewCount],
+						}]).then(function (userResponse) {
 
-			        reviewCount++;
-			        reviewDeck();
-
-					});
-				});
-			} else {
-
-				fs.readFile("clozeLog.txt", function(err, data) {
-
-					var logArray = data.toString().split(",");
-					var deckQuestionArray = [];
-					var deckAnswerArray = [];
-
-					for (var i = 2; i <logArray.length; i+=3) {
-						deckQuestionArray.push(logArray[i].replace("}", ""));
-					};
-
-					for (var i = 1; i <logArray.length; i+=3) {
-						deckAnswerArray.push(logArray[i].replace("}", ""));
-					};
-
-				inquirer.prompt([
-			  		{
-			  			name: "userDisplay",
-			  			message: deckQuestionArray[reviewCount],
-			  		}]).then(function(userResponse){
-			          	
-							if ('"clozeDeletion"' + ":" + '"' + userResponse.userDisplay + '"' == deckAnswerArray[reviewCount]){
+							if ('"back"' + ":" + '"' + userResponse.userDisplay + '"' == deckAnswerArray[reviewCount]) {
 								console.log("YES!");
 							} else {
 								console.log("NO! " + deckAnswerArray[reviewCount]);
@@ -196,9 +163,42 @@ function reviewCards() {
 
 							reviewCount++;
 							reviewDeck();
-			          	}
-					});
+
+						});
 				});
+			} else {
+
+				fs.readFile("clozeLog.txt", function (err, data) {
+
+					var logArray = data.toString().split(",");
+					var deckQuestionArray = [];
+					var deckAnswerArray = [];
+
+					for (var i = 2; i < logArray.length; i += 3) {
+						deckQuestionArray.push(logArray[i].replace("}", ""));
+					};
+
+					for (var i = 1; i < logArray.length; i += 3) {
+						deckAnswerArray.push(logArray[i].replace("}", ""));
+					};
+
+					inquirer.prompt([
+						{
+							name: "userDisplay",
+							message: deckQuestionArray[reviewCount],
+						}]).then(function (userResponse) {
+
+							if ('"clozeDeletion"' + ":" + '"' + userResponse.userDisplay + '"' == deckAnswerArray[reviewCount]) {
+								console.log("YES!");
+							} else {
+								console.log("NO! " + deckAnswerArray[reviewCount]);
+							}
+
+							reviewCount++;
+							reviewDeck();
+						})
+				});
+
 			};
 		};
 	});
